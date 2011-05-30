f2e88e75b865c2669706e887483942aad99fd622
Ben Lings <ben.lings@gmail.com>
2011-05-30 15:10:27 +0100

    Cross compile against 2.9.0.1 and 2.8.1

--- project/build/ScalaGuice.scala
+++ project/build/ScalaGuice.scala
@@ -5,7 +5,15 @@ class ScalaGuice(info: ProjectInfo) extends DefaultProject(info) {
   // Repositories
 
   // Dependencies
-  val scalatest = "org.scalatest" % "scalatest" % "1.3"
   val guice2 = "com.google.inject" % "guice" % "2.0"
+  val TwoPointEight = """2\.8\.\d""".r
+  val TwoPointNine = """2\.9\.[\d.-]+""".r
+
+  val scalatest =
+    buildScalaVersion match {
+      case TwoPointEight() => "org.scalatest" % "scalatest" % "1.3"
+      case TwoPointNine() => "org.scalatest" % "scalatest_2.9.0" % "1.4.1"
+      case x => error("Unsupported Scala version " + x)
+    }
 
 }
